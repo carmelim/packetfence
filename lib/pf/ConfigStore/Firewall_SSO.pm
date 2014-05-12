000aebf4bebf6dd29558f0f2b6dde0db2e5292e8
Zammit Ludovic <lzammit@inverse.ca>
2014-05-12 09:41:19 -0400

    renamed file

--- lib/pf/ConfigStore/Firewall_SSO.pm
+++ lib/pf/ConfigStore/Firewall_SSO.pm
@@ -1,4 +1,4 @@
-package pf::ConfigStore::FirewallSSO;
+package pf::ConfigStore::Firewall_SSO;
 =head1 NAME
 
 pf::ConfigStore::FirewallSSO add documentation
@@ -17,7 +17,37 @@ use Moo;
 use pf::file_paths;
 extends 'pf::ConfigStore';
 
-sub configFile { $pf::file_paths::vlan_firewall_sso_config_file };
+sub configFile { $pf::file_paths::firewall_sso_config_file };
+
+=head2 cleanupAfterRead
+
+Clean up switch data
+
+=cut
+
+sub cleanupAfterRead {
+    my ($self, $id, $profile) = @_;
+    $self->expand_list($profile, $self->_fields_expanded);
+}
+
+=head2 cleanupBeforeCommit
+
+Clean data before update or creating
+
+=cut
+
+sub cleanupBeforeCommit {
+    my ($self, $id, $profile) = @_;
+    $self->flatten_list($profile, $self->_fields_expanded);
+}
+
+=head2 _fields_expanded
+
+=cut
+
+sub _fields_expanded {
+    return qw(categories);
+}
 
 __PACKAGE__->meta->make_immutable;
 
